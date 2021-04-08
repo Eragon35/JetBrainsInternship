bb173602c06899b3d3f7071f4e52a964ade09a86
Eragon35 <antipin35@yahoo.com>
2021-04-08 06:30:25 +0300

    Initial commit

---
+++ src/test/java/com/jetbrains/internship/InternshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jetbrains.internship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
