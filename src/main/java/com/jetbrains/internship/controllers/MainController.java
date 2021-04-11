99190bbda0071cb22bda7ff34fde6c2931ad195e
Eragon35 <antipin35@yahoo.com>
2021-04-12 00:00:20 +0300

    Sending messages

--- src/main/java/com/jetbrains/internship/controllers/MainController.java
+++ src/main/java/com/jetbrains/internship/controllers/MainController.java
@@ -3,14 +3,17 @@ package com.jetbrains.internship.controllers;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.jetbrains.internship.entities.Message;
 import com.jetbrains.internship.entities.Substitution;
 import com.jetbrains.internship.entities.Template;
 import com.jetbrains.internship.repositories.SubstitutionRepository;
 import com.jetbrains.internship.repositories.TemplateRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
@@ -27,14 +30,20 @@ public class MainController {
     }
 
     @PostMapping("/addTemplate")
-    public void add(@RequestBody Template request) {
-        templateRepository.save(request);
-        log.info("Add new {}", request);
+    public ResponseEntity<String> add(@RequestBody Template request) {
+        try {
+            templateRepository.save(request);
+            log.info("Add new {}", request);
+            return new ResponseEntity<>("Template was saved successfully!", HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("Unexpected Error {}", e.getMessage());
+            return new ResponseEntity<>("Unexpected Error: "+ e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
     }
 
 
     @PostMapping("/substitute")
-    public void change(@RequestBody String request) {
+    public ResponseEntity<String> change(@RequestBody String request) {
         JsonObject jsonObject = new JsonParser().parse(request).getAsJsonObject();
         String templateId = jsonObject.get("templateId").toString();
         String variablesString = jsonObject.get("variables")
@@ -44,8 +53,26 @@ public class MainController {
         jsonObject = new JsonParser().parse(variablesString).getAsJsonObject();
         Map variables = new Gson().fromJson(jsonObject, Map.class);
         Substitution substitution = new Substitution(templateId, variables);
-        log.info("new {} arrived", substitution);
-        substitutionRepository.save(substitution);
+        if (jsonObject.has("schedule")){ // make schedule sending
+            substitutionRepository.save(substitution);
+            log.info("new {} arrived", substitution);
+            return new ResponseEntity<>("Substitution was saved successfully and will be sending by schedule", HttpStatus.OK);
+        }
+        else {
+            log.info("new {} arrived", substitution);
+            Template template = templateRepository.findByTemplateId(substitution.getTemplateId());
+            System.out.println(template);
+            Message message = new Message(template, substitution);
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            Gson gson = new Gson();
+            for (String url : template.getRecipients()){
+                HttpEntity<String> response = new HttpEntity<String>(gson.toJson(message), headers);
+                restTemplate.postForEntity(url, message, Message.class);
+            }
+            return new ResponseEntity<>("Message was sent successfully", HttpStatus.OK);
+        }
     }
 
 }
--- src/main/java/com/jetbrains/internship/repositories/TemplateRepository.java
+++ src/main/java/com/jetbrains/internship/repositories/TemplateRepository.java
@@ -4,4 +4,5 @@ import com.jetbrains.internship.entities.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TemplateRepository extends JpaRepository<Template, Long> {
+    Template findByTemplateId(String templateId);
 }
